0bd45461d4f2314cba99a3dccd6f0eae2127fae9
Eric Yang <yangeric12@gmail.com>
2020-04-01 12:08:52 -0400

    initial commit

---
+++ fsTest.js
@@ -0,0 +1,14 @@
+const axios = require("axios")
+const fs = require("fs")
+
+var data = "New File Contents";
+
+fs.writeFile("temp.txt", data, (err) => { // if a buffer is being returned, specify the encoding
+  if (err) console.log(err);
+  console.log("Successfully Written to File.");
+});
+
+fs.readFile("temp.txt", function(err, buf) {
+  if (err) console.log(err)
+  console.log(buf.toString());
+});
