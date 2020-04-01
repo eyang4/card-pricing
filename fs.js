e9cf2ef35a2208c13239e4715a8ad794b69cb983
Eric Yang <yangeric12@gmail.com>
2020-04-01 12:59:28 -0400

    set folder structure

--- fs.js
+++ fs.js
@@ -1,22 +1,36 @@
+const set = "mb1";
+const cards = 1694;
+
 const fs = require("fs");
 const axios = require("axios");
 
-const writeFile = data => {
-  fs.writeFile("temp.txt", data, (err) => { // if a buffer is being returned, specify the encoding
+const writeFile = (location, data) => {
+  fs.writeFile(location, data, (err) => { // if a buffer is being returned, specify the encoding
     if (err) console.error(err);
     console.log("Successfully Written to File.");
   });
 }
 
-const readFile = data => {
+const readFile = location => {
   fs.readFile("temp.txt", function(err, buf) {
     if (err) console.error(err)
     console.log(buf.toString());
   });
 }
 
+if (!fs.existsSync(set)){
+  fs.mkdirSync(set);
+}
+
+const today = new Date().toDateString().replace(/ /g, '-');
+if (!fs.existsSync(set + "/" + today)){
+    fs.mkdirSync(set + "/" + today);
+}
+
+// option + shift + f to format JSON
 axios.get("https://api.scryfall.com/cards/mb1/1")
      .then(val => {
-       writeFile(JSON.stringify(val.data))
+       const data = val.data;
+       writeFile(set + "/" + today + "/" + data.collector_number + ".json", JSON.stringify(data)); // requires directory to exist
      })
      .catch(err => console.error(err));
