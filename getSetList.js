e3468e55f36ba35954c13219f44ed8ad634fa611
Eric Yang <yangeric12@gmail.com>
2020-05-05 16:45:48 -0400

    detail logging

--- getSetList.js
+++ getSetList.js
@@ -1,6 +1,6 @@
 const set = "mb1";
 const delay = 7500;
-let page = 1;
+let page = 38;
 
 const fs = require("fs");
 const axios = require("axios");
@@ -32,20 +32,24 @@ let endTime;
 let uri = "https://api.scryfall.com/cards/search?order=set&unique=prints&q=in%3A" + set + "&page=" + page;
 const storeCardInfo = () => {
   const startTime = new Date();
-  console.log(startTime.toLocaleTimeString() + ", " + startTime.getMilliseconds() + "ms: " + set + " page " + page + " request" + (endTime ? ("; delay " + (startTime - endTime) + " ms") : ""));
+  console.log(startTime.toLocaleTimeString() + ", " + startTime.getMilliseconds() + "ms: " + set + " page " + page + " request" + (endTime ? ("; delay " + (startTime - endTime) + " ms") : "") + "; " + uri);
   axios.get(uri)
        .then(val => {
          endTime = new Date();
-         console.log(endTime.toLocaleTimeString() + ", " + endTime.getMilliseconds() + "ms: " + set + " page " + page + " received");
+         process.stdout.write(endTime.toLocaleTimeString() + ", " + endTime.getMilliseconds() + "ms: " + set + " page " + page + " received");
          page++;
          if (val.status !== 429 && val.data.object === "list" && val.data.next_page) { // check for valid response, otherwise stop requesting
+          // URI verbosely specifies include_extras and include_multilingual to false; default settings
            uri = val.data.next_page;
            setTimeout(storeCardInfo, delay);
          }
          const data = val.data;
          // requires directory to exist
          // overwrites existing file
-         writeFile(set + "/" + today + "/pages/" + (page - 1) + ".json", JSON.stringify(data));
+         const location = set + "/" + today + "/pages/" + (page - 1) + ".json";
+         writeFile(location, JSON.stringify(data));
+         // console.log appends newline to end, stdout.write appends newline to beginning
+         console.log("; " + location + " saved");
        })
        .catch(err => console.error(err));
 }
